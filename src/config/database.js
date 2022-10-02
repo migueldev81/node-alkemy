d4e3671380dcb3c3dba54468c96ad689d7f40eba
migueldev81 <migueldev81@gmail.com>
2022-10-02 20:16:45 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,27 @@ import Sequelize from "sequelize";
 import { config } from "dotenv";
 config()
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-
+export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+   host: process.env.DB_HOST,
+   dialect: 'postgres',
    dialectOptions: {
       ssl: {
          require: true,
          rejectUnauthorized: false
       }
    }
-}
-);
+});
+
+// export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+//    dialectOptions: {
+//       ssl: {
+//          require: true,
+//          rejectUnauthorized: false
+//       }
+//    }
+// }
+// );
 
 sequelize
    .authenticate()
